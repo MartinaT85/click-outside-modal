5f71708609012146a37948dec0636bcc76c37d04
MartinaT85 <86043529+MartinaT85@users.noreply.github.com>
2022-02-08 19:57:14 +0100

    modifying index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import "./styles.css";
 const cardButtons = document.querySelectorAll(".card button");
 
-function handleCardButtonClick() {
-  console.log("you clicked it");
+function handleCardButtonClick(event) {
+  const button = event.currentTarget;
+  const card = button.closest(".card");
+  // img source
+  const imgSrc = card.querySelector("img").src;
+  const desc = card.dataset.description;
 }
 
 cardButtons.forEach((button) => {
