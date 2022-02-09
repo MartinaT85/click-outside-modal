1bb948f8c6587df7545d6e6c8494fbb0f3957244
MartinaT85 <86043529+MartinaT85@users.noreply.github.com>
2022-02-09 10:17:31 +0100

    finnished version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./styles.css";
 const cardButtons = document.querySelectorAll(".card button");
+const modalInner = document.querySelector(".modal-inner");
+const outerModal = document.querySelector(".modal-outer");
 
 function handleCardButtonClick(event) {
   const button = event.currentTarget;
@@ -7,8 +9,30 @@ function handleCardButtonClick(event) {
   // img source
   const imgSrc = card.querySelector("img").src;
   const desc = card.dataset.description;
+
+  // populate modal with the info
+  modalInner.innerHTML = `
+  <img src='${imgSrc.replace("200", "600")}' alt='${desc}' />
+  <p>${desc}</p>
+  `;
+
+  // show modal
+  outerModal.classList.add("open");
 }
 
 cardButtons.forEach((button) => {
   button.addEventListener("click", handleCardButtonClick);
+});
+
+outerModal.addEventListener("click", function (e) {
+  const isOutside = !e.target.closest(".modal-inner");
+  if (isOutside) {
+    outerModal.classList.remove("open");
+  }
+});
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    outerModal.classList.remove("open");
+  }
 });
